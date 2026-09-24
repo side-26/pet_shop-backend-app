a8df66ef19a33f03700c6d0a303ff4c356610c23
Mahdi Rashidi <mr72302@gmail.com>
2026-09-24 16:40:06 +0330

    fix: add response and input to the open-api and scalar

--- src/integrations/reverseGeocoding/reverseGeocoding.route.js
+++ src/integrations/reverseGeocoding/reverseGeocoding.route.js
@@ -11,6 +11,7 @@ const router = express.Router();
 router.get(
   REVERSE_GEOCODING_ROUTES.reverseGeocode,
   authenticated,
+  /* #swagger.responses[200] = { description: 'Reverse-geocoded location', content: { "application/json": { schema: { type: 'object', required: ['isSuccess', 'data'], properties: { isSuccess: { type: 'boolean', example: true }, data: { type: 'object', required: ['formatted_address'], properties: { formatted_address: { type: 'string', example: 'تهران، خیابان فاطمی' }, city: { type: 'string', example: 'تهران' }, state: { type: 'string', example: 'استان تهران' } }, additionalProperties: true } }, example: { isSuccess: true, data: { formatted_address: 'تهران، خیابان فاطمی', city: 'تهران', state: 'استان تهران' } } } } } } */
   reverseGeocodeController,
 );
 
